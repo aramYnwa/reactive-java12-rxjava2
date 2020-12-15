800c788ce86e30746ec6f5f31133fb7f358b74d2
Aramais Khachatryan <rtgkid8@gmail.com>
2020-12-16 03:25:48 +0400

    Initial setup

---
+++ src/main/java/org/example/rxjava/EventSequenceExample.java
@@ -0,0 +1,15 @@
+package org.example.rxjava;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EventSequenceExample {
+
+  private static Logger log = LoggerFactory.getLogger(EventSequenceExample.class);
+
+  public static void main(String[] args) {
+
+  }
+
+}
