a8d15291bdc9e6f342cf3e7f1c5ba21283ec42dc
Aramais Khachatryan <rtgkid8@gmail.com>
2020-12-16 04:16:58 +0400

    Add simple observable and observer example

--- src/main/java/org/example/rxjava/EventSequenceExample.java
+++ src/main/java/org/example/rxjava/EventSequenceExample.java
@@ -1,16 +1,60 @@
 package org.example.rxjava;
 
-
 import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 import org.example.rxjava.utility.GateBasedSynchronization;
+import org.example.rxjava.utility.datasets.GreekAlphabet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class EventSequenceExample {
 
-  private static Logger log = LoggerFactory.getLogger(EventSequenceExample.class);
+  private static Logger LOG = LoggerFactory.getLogger(EventSequenceExample.class);
 
   public static void main(String[] args) {
+
+    // This sync tool is meant for keeping away from exiting
+    // till all test code has been executed.
+    GateBasedSynchronization gate = new GateBasedSynchronization();
+
+    // Create an Observable<String> that contains greek alphabet
+    // Create a simple observer and implement methods
+
+    Observable.fromArray(GreekAlphabet.greekLetters)
+      .subscribe(new Observer<>() {
+
+        @Override
+        public void onSubscribe(Disposable disposable) {
+          LOG.info("onSubscribe");
+        }
+
+        @Override
+        public void onNext(String nextLetter) {
+          LOG.info("onNext - {}", nextLetter);
+        }
+
+        @Override
+        public void onError(Throwable throwable) {
+          LOG.error("onError - {}", throwable.getMessage());
+
+          // To let main thread go on we gonna open gait for "onError"
+          gate.openGate("onError");
+        }
+
+        // This is called when the Observable finishes emitting all events.
+        // If onError is called this won't be called.
+        // Likewise if this is called, onError won't happen.
+        @Override
+        public void onComplete() {
+          LOG.info("onComplete");
+          gate.openGate("onComplete");
+        }
+      });
+
+    gate.waitForAny("onComplete", "onError");
+
+    System.exit(0);
 
   }
 
